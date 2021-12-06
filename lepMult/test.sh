1278ea6826a0ec3a12f7f77ecb06b91791dccaf2
John Stupak <jstupak@cern.ch>
2021-12-06 13:58:42 -0600

    commiting BS to test on SC

--- lepMult/test.sh
+++ lepMult/test.sh
@@ -4,18 +4,14 @@ if [[ `basename $PWD` != "MCProd" ]]; then echo "Execute from MCProd dir"; exit;
 
 source setup.sh
 
-#rm -rf MG5_aMC_v3_1_1
-#bzr branch lp:~maddevelopers/mg5amcnlo/madspin_fake_br MG5_aMC_v3_1_1
-
-#cp -r lepMult/lepMultBias/ MG5_aMC_v3_1_1/Template/LO/Source/BIAS/lepMult
-
 cd MG5_aMC_v3_1_1
-rm -rf step*
+#rm -rf step*
+rm -rf step2
 
-python ./bin/mg5_aMC < ../lepMult/step1.mg
+#python ./bin/mg5_aMC < ../lepMult/step1.mg
 python ./bin/mg5_aMC < ../lepMult/step2.mg
 
-gunzip step1/Events/run_0?/unweighted_events.lhe.gz
+#gunzip step1/Events/run_0?/unweighted_events.lhe.gz
 gunzip step2/Events/run_0?_decayed_1/unweighted_events.lhe.gz
 
 echo
